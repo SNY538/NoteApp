1df9eca8768c5c715b2cfeb2d8284fb89c6b27de
nikita.semyonov.00@list.ru <nikita.semyonov.00@list.ru>
2020-11-08 22:30:59 +0700

    Разработана логика приложения

---
+++ NoteAppUI/NoteApp/ProjectManager.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace NoteApp
+{
+    /// <summary>
+    /// Сериализация заметок
+    /// </summary>
+    public class ProjectManager
+    {
+        public static void WritingToFile(Project project, string fileName)
+        {
+            JsonSerializer serializer = new JsonSerializer()
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+            using (StreamWriter sw = new StreamWriter(fileName))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, project);
+            }
+        }
+
+        public static Project ReadingFromFile (string fileName)
+        {
+            Project project = null;
+            JsonSerializer serializer= new JsonSerializer()
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.All
+            };
+            using (StreamReader sr= new StreamReader(@"C:\json.txt"))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                project = (Project) serializer.Deserialize<Project>(reader);
+            }
+
+            return project;
+        }
+
+    }
+}
--- NoteAppUI/NoteAppUI/Form1.cs
+++ NoteAppUI/NoteAppUI/Form1.cs
@@ -8,14 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using NoteApp;
+using System.IO;
 
 namespace NoteAppUI
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Project project = new Project();
+            Note note = new Note("Тестовая заметка", Category.Other, "Заметка для теста", DateTime.Now);
+            project.Glossary.Add(note);
+            Console.WriteLine(note.Name + "  " + note.Category + "  " + note.Text + "  " + note.TimeCreation);
+         
         }
     }
 }
